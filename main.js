7be5ed5f7a404cc73dcb0552c5cfe591a8a67505
Ryder <rdeback@gmail.com>
2021-06-25 16:37:40 -0700

    Got all users put into a map to query later.

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
+'use strict';
 const Discord = require('discord.js');
 const token = process.env.IMGTOKEN;
+const serverID = process.env.BOTSERVERID;
 
-const client = new Discord.Client();
-
-
-
-
-
-
-
-
+const client = new Discord.Client({fetchAllMembers:true});
 
 client.once('ready', () => {
+    const list = client.guilds.cache.get(serverID);
+    const userNames = list.members.cache.map(member => member.user.username)
     console.log("The bot is online!");
+    console.log(userNames);
 })
-
 
 client.login(token);
