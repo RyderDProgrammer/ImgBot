b7c4001093155a841f55616dad13613e944ec7a0
Ryder <rdeback@gmail.com>
2021-06-25 20:12:50 -0700

    Made global array for later access.

--- main.js
+++ main.js
@@ -2,18 +2,24 @@
 const Discord = require('discord.js');
 const token = process.env.IMGTOKEN;
 const serverID = process.env.BOTSERVERID;
-const prefix = "*";
+const prefix = "/";
+
+let allUsersArray = new Array();
 
 const client = new Discord.Client({fetchAllMembers:true});
 
 client.once('ready', () => {
     const list = client.guilds.cache.get(serverID);
-    const userNames = list.members.cache.map(member => member.user.id)
+    
+    //Adding all the users to a global constant.
+    //Making it able to be used in other functions.
+    allUsersArray = list.members.cache.map(member => member.user.id);
     console.log("The bot is online!");
-    console.log(userNames);
+    console.log(allUsersArray);
 })
 
 client.on("message",msg => {
+    
     if(msg.author.bot) return;
     if(msg.content.startsWith(prefix + "test"))
     {
