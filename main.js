d2ba1653b5e330ec33804da4b85827e9e5321093
Ryder <rdeback@gmail.com>
2021-06-27 18:50:19 -0700

    Fixed an array issue and made it into a function.

--- main.js
+++ main.js
@@ -7,26 +7,25 @@ const prefix = "!";
 //A way to store both userId's and userNames into arrays.
 //They are 1 to 1 so the 3rd position in IdArray is the same person
 //as the 3rd position in the NameArray.
-let userIdArray = new Array();
-let userNameArray = new Array();
+var userIdArray = new Array();
+var userNameArray = new Array();
+var userNickArray = new Array();
 
 const validImgQuery = ["i","im","img","imag","image","v","vi","vid","vide","video"];
 
-let imgArray = new Array();
-let vidArray = new Array();
+var imgArray = new Array();
+var vidArray = new Array();
 
 const client = new Discord.Client({fetchAllMembers:true});
 
 client.once('ready', () => {
-    const list = client.guilds.cache.get(serverID);
-
     //Adding all the users the global constants.
     //Making it able to be used in other functions.
-    userIdArray = list.members.cache.map(member => member.user.id);
-    userNameArray = list.members.cache.map(member => member.user.username);
+    loadAllUserInfo();
     console.log("The bot is online!");
-    //console.log(userIdArray);
-    //console.log(userNameArray);
+    // console.log(userIdArray);
+    // console.log(userNameArray);
+    // console.log(userNickArray);
 })
 
 client.on("message",msg => {
@@ -34,10 +33,11 @@ client.on("message",msg => {
     let userMsg = msg.content.slice(1);
     let msgUserName = userMsg.split(' ')[0];
     let imgOrVid = userMsg.split(' ')[1];
+
+    loadAllUserInfo();
     
-    console.log(msgUserName);
-    console.log(usernameInIDArray(msgUserName));
-    //console.log(imgOrVid);
+    let imgFilter = m => msg.attachments.size > 0 && isImage(msg);
+    let imgCollect = new Discord.MessageCollector(msg.channel,imgFilter);
 
     if(msg.content.startsWith(prefix))
     {
@@ -45,7 +45,8 @@ client.on("message",msg => {
         {
             msg.channel.send("This is a test of the bot");
         }
-        else if(userNameArray.includes(msgUserName) && validImgQuery.includes(imgOrVid))
+        else if((userIdArray.includes(usernameInIDArray(msgUserName)) || userIdArray.includes(nicknameInIDArray(msgUserName))) 
+                && validImgQuery.includes(imgOrVid) )
         {
             if(validImgQuery.indexOf(imgOrVid) < 5)
             {
@@ -66,8 +67,37 @@ client.on("message",msg => {
 function usernameInIDArray(name)
 {
     let idAsName = userNameArray.indexOf(name);
+    console.log(idAsName);
     let nameAsID = userIdArray[idAsName];
+    console.log(nameAsID);
     return nameAsID;
+}
+
+function nicknameInIDArray(nick)
+{
+    let idAsNick = userNickArray.indexOf(nick);
+    console.log(idAsNick);
+    let nameAsID = userIdArray[idAsNick];
+    console.log(nameAsID);
+    return nameAsID;
+}
+
+function loadAllUserInfo()
+{
+    const list = client.guilds.cache.get(serverID);
+
+    userIdArray = list.members.cache.map(member => member.user.id);
+    userNameArray = list.members.cache.map(member => member.user.username);
+    userNickArray = list.members.cache.map(member => member.nickname);
+
+    console.log(userIdArray);
+    console.log(userNameArray);
+    console.log(userNickArray);
+}
+
+function isImage()
+{
+
 }
 
 client.login(token);
